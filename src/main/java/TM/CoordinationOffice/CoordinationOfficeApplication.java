71c53f24586ff81d86b57aa5a3298ae0b29f2293
sze1totmih <sze1totmih@vasvari.org>
2023-08-16 11:30:23 +0200

    Coordination Office project start

---
+++ src/main/java/TM/CoordinationOffice/CoordinationOfficeApplication.java
@@ -0,0 +1,13 @@
+package TM.CoordinationOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinationOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinationOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TM/CoordinationOffice/CoordinationOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package TM.CoordinationOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinationOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
