d70161cd0889b7d333fa6227ce9ed7c3908968dc
sze1totmih <sze1totmih@vasvari.org>
2023-08-16 11:58:53 +0200

    Role class/model start

---
+++ src/main/java/TM/CoordinationOffice/model/role.java
@@ -0,0 +1,18 @@
+package TM.CoordinationOffice.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class role {
+
+    private int roleID;
+    @NotBlank
+    private int roleName;
+}
