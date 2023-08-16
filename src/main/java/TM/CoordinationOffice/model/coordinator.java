f4a10b92f5fc0f15b766ea9d5572220e84055073
sze1totmih <sze1totmih@vasvari.org>
2023-08-16 12:06:41 +0200

    coordinator class/model start

---
+++ src/main/java/TM/CoordinationOffice/model/coordinator.java
@@ -0,0 +1,28 @@
+package TM.CoordinationOffice.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class coordinator {
+
+    private int coordinatorId;
+    @NotBlank
+    @Size(min = 3, max=50)
+    private String coordinatorName;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+    private boolean backup;
+    private boolean blind;
+}
